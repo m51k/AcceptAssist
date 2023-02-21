faaeb0c905c3d79f3c77d58d3fa28208f3d13c88
GSujiKanth <119513869+GSujiKanth@users.noreply.github.com>
2023-02-21 09:39:23 +0530

    modified some codes

--- web_scraping_uni_rank.py
+++ web_scraping_uni_rank.py
@@ -19,21 +19,23 @@ print(soup.title.name)
 print(soup.title.parent.name)
 
 # Find the HTML element with class name "row ind "
-university_blocks = soup.find_all('div', class_='row')
+university_blocks = soup.find_all('div', class_="row")
 
-for university_block in university_blocks:
-    print(university_block)
+# for university_block in university_blocks:
+#     print(university_block)
+
+print(university_blocks)
 
 
 
-# # Extract the information for each university
-# for block in university_blocks:
-#     name = block.find("a", class_="uni-link")
-#     location = block.find("div", class_="location ")
-#     world_rank = block.find("div", class_="_univ-rank hide-this-in-mobile-indi ")
-#
-#     # Print the extracted information
-#     print("Name:", name)
-#     print("Location:", location)
-#     print("World Rank:", world_rank)
-#     print("---")
+# Extract the information for each university
+for block in university_blocks:
+    name = block.find("a", class_="uni-link")
+    location = block.find("div", class_="location ")
+    world_rank = block.find("div", class_="_univ-rank hide-this-in-mobile-indi ")
+
+    # Print the extracted information
+    print("Name:", name)
+    print("Location:", location)
+    print("World Rank:", world_rank)
+    print("---")
