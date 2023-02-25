cc51b6849ab624d5a04404d2d61620a626764f39
Pasan Diksura <pasan.20210415@iit.ac.lk>
2023-02-26 01:08:48 +0530

    Update

---
+++ Code/Login and Register Pages/PHP/config.php
@@ -0,0 +1,5 @@
+<?php 
+
+    $con = mysqli_connect("localhost","Users","pasan_diksura","Documents","Software_Development","GitHub","Acceptassist","Code","Login and Register Pages") or die("Couldn't connect")
+
+?>
