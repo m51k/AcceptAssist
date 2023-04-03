8b3f42b1e500c34f3f03e58d197ac040762a0a4e
m51k <mekail@gmail.com>
2023-04-03 10:46:10 +0530

    Added API call to predict acceptance (still testing)

--- Code/User Page/userscore.php
+++ Code/User Page/userscore.php
@@ -13,11 +13,11 @@ echo "<h4>".$pagename."</h4>";
 //Capture and trim the 7 inputs entered in the the 7 fields of the form using the $_POST superglobal variable
 //Store these details into a set of 7 new local variables
 
-function postUserScores($url, $data) {
+function apiCall($endpoint, $data) {
     $init = curl_init();
 
     curl_setopt_array($init, array(
-        CURLOPT_URL => $url,
+        CURLOPT_URL => $endpoint,
         CURLOPT_CUSTOMREQUEST => "POST",
         CURLOPT_POSTFIELDS => $data,
       ));
@@ -42,6 +42,12 @@ $userScores = array(
     'r_lor' => trim($_POST['r_lor']),
     'r_cgpa' => trim($_POST['r_cgpa'])
 );
+
+$rating = 3;
+$url = 'http://localhost:8000/api/acceptance-prediction/'.$rating.'/';
+$prediction = apiCall($url, $userScores);
+
+echo "<p>Prediction: $prediction</p>";
 // GRE Score	TOEFL Score	University Rating	SOP	LOR	CGPA	Research	Chance of Admit
 
 include("../Navigation Bar & Footer/Footer.html");
