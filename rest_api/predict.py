40d485f26bfad79568d0715938a3ad74f4e2e78b
m51k <mekail@gmail.com>
2023-04-04 08:56:26 +0530

    Moved code for prediction into predict.py

---
+++ rest_api/predict.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import numpy as np
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split
+
+def randomForestRegressor(gre, toefl, rating, sop, lor, cgpa, research):
+    data = pd.read_csv('../ML/admit.csv')
+    X = data.iloc[:, 1:8] # features
+    y = data.iloc[:, 8] # target
+    
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+    
+    regressor = RandomForestRegressor(n_estimators=100, max_depth=5)
+    
+    regressor.fit(X_train, y_train)
+    
+    prediction = regressor.predict([[gre, toefl, rating, sop, lor, cgpa, research]])
+    
+    return prediction
--- rest_api/views.py
+++ rest_api/views.py
@@ -1,37 +1,20 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
-
-import pandas as pd
-import numpy as np
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.model_selection import train_test_split
+from rest_framework import status
+from rest_api.predict import randomForestRegressor
 
 # Create your views here.
 class PredictAcceptance(APIView):
-    def post(self, request, rating):
-        gre = request.data.get('r_grescore')
-        toefl = request.data.get('r_toeflscore')
-        sop = request.data.get('r_sop')
-        lor = request.data.get('r_lor')
-        cgpa = request.data.get('r_cgpa')
-        research = request.data.get('r_research')
+    def post(self, request):
+        gre = request.POST.get('r_grescore')
+        toefl = request.POST.get('r_toeflscore')
+        sop = request.POST.get('r_sop')
+        lor = request.POST.get('r_lor')
+        cgpa = request.POST.get('r_cgpa')
+        research = request.POST.get('r_research')
+        rating = request.POST.get('r_rating')
         
         prediction = randomForestRegressor(gre, toefl, rating, sop, lor, cgpa, research)
 
-        return prediction
-    
-def randomForestRegressor(gre, toefl, rating, sop, lor, cgpa, research):
-        data = pd.read_csv('../ML/admit.csv')
-        X = data.iloc[:, 1:8] # features
-        y = data.iloc[:, 8] # target
-        
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
-        
-        regressor = RandomForestRegressor(n_estimators=100, max_depth=5)
-        
-        regressor.fit(X_train, y_train)
-        
-        prediction = regressor.predict([[gre, toefl, rating, sop, lor, cgpa, research]])
-        
-        return prediction
+        return Response(prediction[0], status=status.HTTP_200_OK)
