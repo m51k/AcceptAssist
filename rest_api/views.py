72b0dcfefd1343dcbf4732506664918ff4e3e4fc
m51k <mekail@gmail.com>
2023-04-04 09:53:09 +0530

    Removed unecessary page and made api call within page

--- rest_api/views.py
+++ rest_api/views.py
@@ -6,14 +6,26 @@ from rest_api.predict import randomForestRegressor
 
 # Create your views here.
 class PredictAcceptance(APIView):
+    # def post(self, request):
+    #     gre = request.data.get('r_grescore')
+    #     toefl = request.data.get('r_toeflscore')
+    #     sop = request.data.get('r_sop')
+    #     lor = request.data.get('r_lor')
+    #     cgpa = request.data.get('r_cgpa')
+    #     research = request.data.get('r_research')
+    #     rating = request.data.get('r_rating')
+        
+    #     prediction = randomForestRegressor(gre, toefl, rating, sop, lor, cgpa, research)
+
+    #     return Response(prediction[0], status=status.HTTP_200_OK)
     def post(self, request):
-        gre = request.POST.get('r_grescore')
-        toefl = request.POST.get('r_toeflscore')
-        sop = request.POST.get('r_sop')
-        lor = request.POST.get('r_lor')
-        cgpa = request.POST.get('r_cgpa')
-        research = request.POST.get('r_research')
-        rating = request.POST.get('r_rating')
+        gre = 337
+        toefl = 118
+        sop = 4.5
+        lor = 4.5
+        cgpa = 9.65
+        research = 1
+        rating = 4
         
         prediction = randomForestRegressor(gre, toefl, rating, sop, lor, cgpa, research)
 
