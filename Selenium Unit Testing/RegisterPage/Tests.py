fdd852b13c7ae5631d9d3adb7a8f093f1a4dfc78
m51k <mekail@gmail.com>
2023-04-06 00:29:38 +0530

    removed unecessary line

--- Selenium Unit Testing/RegisterPage/Tests.py
+++ Selenium Unit Testing/RegisterPage/Tests.py
@@ -1,7 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 
